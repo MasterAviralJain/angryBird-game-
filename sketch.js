b2ee87694ce3a5eac4890d03095e06e197a77f29
MasterAviralJain <aviraljain1309@gmail.com>
2021-05-30 10:35:59 +0530

    aviral

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,conlog,chain,Alog,chain2;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +38,14 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    conlog=new Log(180,180,80,PI/2);
+
+    chain=new Chain(bird.body,conlog.body);
+
+    Alog=new Log(180,100,100,40);
+
+    chain2=new Chain(Alog.body,conlog.body);
+
 }
 
 function draw(){
@@ -61,4 +71,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    conlog.display();
+    chain.display();
+    Alog.display();
+    chain2.display();
 }
