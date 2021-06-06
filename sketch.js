ee3622a24ae6de20406b4fcfefbca8a05786a0f9
MasterAviralJain <aviraljain1309@gmail.com>
2021-06-06 10:42:58 +0530

    slingshot is made

--- sketch.js
+++ sketch.js
@@ -36,11 +36,12 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(200,50);
 
     
 
-    slingshot=new SlingShot(bird.body,{x:200,y:100});
+    slingshot=new SlingShot(bird.body,{x:200,y:50});
+    
 
    
 
